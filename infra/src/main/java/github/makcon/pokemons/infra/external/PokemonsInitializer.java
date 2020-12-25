69a04a1b981f6e037b5f66041128ca23272a4bbb
mmatlakhov <mmatlakhov@pagantis.com>
2020-12-25 12:51:38 +0100

    call initializer if pokemons repository is empty

---
+++ infra/src/main/java/github/makcon/pokemons/infra/external/PokemonsInitializer.java
@@ -0,0 +1,11 @@
+package github.makcon.pokemons.infra.external;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PokemonsInitializer {
+
+    public void init() {
+
+    }
+}
